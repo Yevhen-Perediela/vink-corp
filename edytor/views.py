1129fd576274bb1f626d3967cf781fe82e785339
Yevhen Perediela <peredelaevgen23@gmail.com>
2025-04-16 22:44:28 +0200

    basic edytor, basic frontend, static files

--- edytor/views.py
+++ edytor/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
 def edytor(request):
-    return render(request, 'index.html')
+    return render(request, 'edytor/edytor.html')
---
+++ static/edytor.js
@@ -0,0 +1,19 @@
+require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs' }});
+  require(['vs/editor/editor.main'], function () {
+    window.editor = monaco.editor.create(document.getElementById('editor'), {
+      value: `// Hello, Vink 👋\nconsole.log("Edit me!");`,
+      language: 'javascript',
+      theme: 'vs-dark',
+      automaticLayout: true,
+    });
+  });
+
+  // 🚀 Funkcja do pobrania kodu z edytora do zmiennej
+  function getCodeFromEditor() {
+    return window.editor.getValue();
+  }
+
+  // 🔥 Funkcja do ustawiania nowego kodu do edytora
+  function setCodeInEditor(code) {
+    window.editor.setValue(code);
+  }
