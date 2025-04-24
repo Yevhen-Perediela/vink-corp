bd0ae2695d19eef06a2815aa4fe1862c65416ecb
Herman <herman.papko@gmail.com>
2025-04-24 13:34:53 +0200

    auth is ready without styles

--- main/auth/forms.py
+++ main/auth/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
-class LoginForm(forms.Form):
-    username = forms.CharField(required=True, label='Nazwa użytkownika')
-    password = forms.CharField(required=True, label='Hasło', widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
