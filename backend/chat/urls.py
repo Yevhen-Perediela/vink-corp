90e567457f1c777ccd378045ef54bd0b63c8e421
Yevhen Perediela <peredelaevgen23@gmail.com>
2025-04-24 13:52:30 +0200

    delete "Pierwsze coś"

--- backend/chat/urls.py
+++
@@ -1,7 +0,0 @@
-# chat/urls.py
-from django.urls import path
-from .views import ChatMessageListCreateView
-
-urlpatterns = [
-    path('', ChatMessageListCreateView.as_view(), name='chat-messages'),
-]
